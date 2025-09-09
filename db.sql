32c4bdd259b1e1cd704acff9eebdc18e9db64e32
Your Name~ <you@example.com>
2025-09-09 21:43:50 +0300

    created train_station table with id and station_name

--- db.sql
+++ db.sql
@@ -3,3 +3,8 @@ CREATE DATABASE trainbookingDB;
 
 -- Use the database
 USE trainbookingDB;
+
+CREATE TABLE train_station(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  station_name VARCHAR(255) NOT NULL
+);
