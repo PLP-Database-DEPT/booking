30b8068b4031d764df8317e7ba6c5407c0806f31
theespeedninja <dannygitonga1@gmail.com>
2025-09-09 19:30:37 +0300

    Created the journey_station table

--- db.sql
+++ db.sql
@@ -3,3 +3,13 @@ CREATE DATABASE trainbookingDB;
 
 -- Use the database
 USE trainbookingDB;
+
+-- Creating the journey_Station table
+CREATE TABLE journey_station (
+    journey_id INT,
+    station_id INT,
+    stop_order INT,
+    departure_time DATETIME,
+    FOREIGN KEY (journey_id) REFERENCES train_journey(journey_id), -- foreign key to the journey_id in the train_journey table
+    FOREIGN KEY (station_id) REFERENCES train_station(station_id) -- foreign key to the station_id in the train_station table
+);
