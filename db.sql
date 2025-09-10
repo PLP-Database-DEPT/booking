32a92bf901728cc9b3b7c2beeaa41d92715f3c8c
theespeedninja <dannygitonga1@gmail.com>
2025-09-10 08:32:04 +0300

    Created the journey_station table and now the schedule table created

--- db.sql
+++ db.sql
@@ -13,3 +13,9 @@ CREATE TABLE journey_station (
     FOREIGN KEY (journey_id) REFERENCES train_journey(journey_id), -- foreign key to the journey_id in the train_journey table
     FOREIGN KEY (station_id) REFERENCES train_station(station_id) -- foreign key to the station_id in the train_station table
 );
+
+-- creating the schedule table
+CREATE TABLE schedule (
+	id INT PRIMARY KEY,
+    name VARCHAR (50)
+);
