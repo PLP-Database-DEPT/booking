45168e32df97f09e8329e85d03e054f839704e96
drBILAL-mj <bilalmjdr33@gmail.com>
2025-09-10 15:22:57 +0300

    initial commit

--- db.sql
+++ db.sql
@@ -3,3 +3,12 @@ CREATE DATABASE trainbookingDB;
 
 -- Use the database
 USE trainbookingDB;
+
+-- Create Table for Passenger
+CREATE TABLE passenger (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email_address VARCHAR(100),
+    password VARCHAR(100)
+);
