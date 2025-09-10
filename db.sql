bbea6cd601e16d743ca249c90743806487e353f7
Dee-GeoDev22 <moyadamacline@gmail.com>
2025-09-10 18:38:18 +0300

    Tackled issue #6

--- db.sql
+++ db.sql
@@ -11,4 +11,18 @@ class_name VARCHAR(255) NOT NULL,
 seating_capacity INT NOT NULL
 );
 
+-- Creating journey_carriage table
+CREATE TABLE journey (
+id INT PRIMARY KEY,
+journey_date DATE NOT NULL
+);
+
+CREATE TABLE journey_carriage (
+    journey_id INT NOT NULL,           
+    carriage_class_id INT NOT NULL,    
+    position INT NOT NULL,             
+    FOREIGN KEY (journey_id) REFERENCES journey(id),
+    FOREIGN KEY (carriage_class_id) REFERENCES carriage_class(id)
+);
+
 
