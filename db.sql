d94eb53456dd554e60a616a50491f6e59d800aff
drBILAL-mj <bilalmjdr33@gmail.com>
2025-09-10 16:42:21 +0300

    updated queries fourth table created

--- db.sql
+++ db.sql
@@ -40,3 +40,13 @@ CREATE TABLE booking (
     FOREIGN KEY (train_journey_id) REFERENCES train_journey(id),
     FOREIGN KEY (ticket_class_id) REFERENCES ticket_class(id)
 );
+
+-- Create the carriage_price table
+CREATE TABLE carriage_price (
+    schedule_id INT,
+    carriage_class_id INT,
+    price DECIMAL(10, 2),
+    PRIMARY KEY (schedule_id, carriage_class_id),
+    FOREIGN KEY (schedule_id) REFERENCES schedule(id),
+    FOREIGN KEY (carriage_class_id) REFERENCES carriage_class(id)
+);
