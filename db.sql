a2e8146eb34ac823dfc207804ca10a11cb573444
Dee-GeoDev22 <moyadamacline@gmail.com>
2025-09-10 16:28:02 +0300

    Created carriage_class table

--- db.sql
+++ db.sql
@@ -3,3 +3,12 @@ CREATE DATABASE trainbookingDB;
 
 -- Use the database
 USE trainbookingDB;
+
+-- Creating carriage_class table
+CREATE TABLE carriage_class (
+id INT PRIMARY KEY,                 -- Unique identifier for each carriage class
+class_name VARCHAR(255) NOT NULL,
+seating_capacity INT NOT NULL
+);
+
+
